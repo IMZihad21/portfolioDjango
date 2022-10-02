2358ab4c139c96d77ac1b0dc5e4743d0bf7446e4
ZèD <imzihad@gmail.com>
2022-10-02 10:29:58 +0600

    home: added landing from github
    Signed-off-by: ZèD <imzihad@gmail.com>

--- core/settings.py
+++ core/settings.py
@@ -47,18 +47,22 @@ ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    #
-    # django apps
-    #
+]
+
+THIRD_PARTY_APPS = []
+
+LOCAL_APPS = [
     "home.apps.HomeConfig",
 ]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
+import requests
 from django.shortcuts import render
 
+GITHUB_PROFILE = "https://raw.githubusercontent.com/IMZihad21/IMZihad21/main/README.md"
 
 # Create your views here.
 def HomeView(request):
-    context = {}
+    github_profile = requests.get(GITHUB_PROFILE)
+    context = {"github_profile": github_profile.text}
     return render(request, "home/index.html", context)
