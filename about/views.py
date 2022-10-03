25ae91c44c8a11dfea1122ba280a041d48b23edc
ZèD <imzihad@gmail.com>
2022-10-03 20:33:29 +0600

    about: get about data from gist for now
    Signed-off-by: ZèD <imzihad@gmail.com>

--- about/views.py
+++ about/views.py
@@ -4,5 +4,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def AboutView(request):
-    context = {}
+    profile_data = requests.get(
+        "https://gist.githubusercontent.com/IMZihad21/87c7211efa951e1f10b4d4f5c89fc5d5/raw/portfolioData.json"
+    ).json()
+    context = {"about_data": profile_data["aboutMe"]}
     return render(request, "about/about.html", context)
