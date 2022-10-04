00ecd05c5c12c9b8d85cc00705b5dd3c74dccba6
ZèD <imzihad@gmail.com>
2022-10-04 10:54:30 +0600

    projects: created project model
    Signed-off-by: ZèD <imzihad@gmail.com>

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,21 @@
+from core.models import BaseModel
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+
+class Project(BaseModel):
+    """
+    Project model defines project information ie: name, description, links etc.
+    """
+
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=2000)
+    cover = models.CharField(max_length=200)
+    screenshots = ArrayField(
+        models.CharField(max_length=200),
+        size=8,
+    )
+    tags = ArrayField(
+        models.CharField(max_length=100),
+        size=8,
+    )
