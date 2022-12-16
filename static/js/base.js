db78f1116804d5a3acf4a174556849a266ae272b
ZèD <imzihad@gmail.com>
2022-12-16 19:42:39 +0600

    alpine: nuke alpinejs from project
    Signed-off-by: ZèD <imzihad@gmail.com>

--- static/js/base.js
+++ static/js/base.js
@@ -1,14 +1,14 @@
 // default fetch function
 const fetchAPI = async (apiURL, responseType = "json") => {
-    const response = await fetch(apiURL)
-    switch (responseType) {
-        case "text":
-            return await response.text()
+  const response = await fetch(apiURL);
+  switch (responseType) {
+    case "text":
+      return await response.text();
 
-        case "blog":
-            return await response.blob()
+    case "blob":
+      return await response.blob();
 
-        default:
-            return await response.json()
-    }
-}
+    default:
+      return await response.json();
+  }
+};
